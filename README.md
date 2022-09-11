e0007067ce5571320fd3675a768b2c224f6ec7e8
ybw0014 <i@guizhanss.com>
2022-09-10 21:17:18 -0400

    翻译

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
-# Slimefun4 Addon
-This is an example Repository for a generic Slimefun4 Addon.
-In the top left is a button "Use this template", click this to create your own Addon for Slimefun4 using this basic template.
+# 粘液科技附属 - 模版
+这是一个简单的粘液科技附属模版。在右上角有一个绿色的"Use this template"按钮，点击该按钮使用该模版来创建你的附属。
 
-## How to create your own addon.
-This is a template repository that you can use to create your own Slimefun4 Addon.<br>
-We have also written an extensive step-by-step tutorial which you can find here:<br>
-https://github.com/Slimefun/Slimefun4/wiki/Developer-Guide
+## 如何创建你的附属
+这是一个模版仓库，你可以用它来创建你自己的粘液科技附属。  
+我们也编写了一个较为详细的、循序渐进的开发指南，你可以在此查看：  
+https://slimefun-wiki.guizhanss.cn/Developer-Guide
 
-## Changing some important things
-Navigate to `src/main/java` and rename the package and the .java File to your liking.<br>
-Suggestion: "me.yourname.yourproject" (all lower case) and "ProjectName.java"<br>
-Example: "me.thebusybiscuit.cooladdon" and "CoolAddon.java"
+## 一些重要的部分需要修改
+打开文件夹 `src/main/java` 并重命名包名和主类名。  
+建议: "me.你的名称.你的项目" (全部小写) 以及 "项目名称.java"  
+例如: "me.ybw0014.cooladdon" 以及 "CoolAddon.java"
 
-Navigate to `src/main/resources/plugin.yml` and change the "author" and "main" attributes.
-You may also want to change the description to something meaningful.
+打开文件 `src/main/resources/plugin.yml` 并更改 "author" 为你的名字，"main" 为主类的路径。  
+你可能还需要改变"description"并提供一个有意义的描述。
 
-Navigate to `pom.xml` and change the group id to "me.%Your name%" and change the artifact id to the name of your Project.
+打开文件 `pom.xml` 并更改 group id 为 "me.%你的名字%" 并修改 artifact id 为你项目的名称。
 
-After that you are good to go, you can now start developing your own Addon for Slimefun4.
+做完这些，你就可以开始编写剩余的部分了。
--- src/main/java/me/CHANGEME/slimefunaddon/ExampleAddon.java
+++ src/main/java/me/CHANGEME/slimefunaddon/ExampleAddon.java
@@ -17,45 +17,44 @@ public class ExampleAddon extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
-        // Read something from your config.yml
+        // 从 config.yml 中读取插件配置
         Config cfg = new Config(this);
 
         if (cfg.getBoolean("options.auto-update")) {
-            // You could start an Auto-Updater for example
+            // 你可以在这里添加自动更新功能
         }
 
         /*
-         * 1. Creating a new Category
-         * This Category will use the following ItemStack
+         * 1. 创建分类
+         * 分类的显示物品将使用以下物品
          */
-        ItemStack itemGroupItem = new CustomItemStack(Material.DIAMOND, "&4Addon Category", "", "&a> Click to open");
+        ItemStack itemGroupItem = new CustomItemStack(Material.DIAMOND, "&4附属分类");
 
-        // Give your Category a unique id.
+        // 给你的分类提供一个独一无二的ID
         NamespacedKey itemGroupId = new NamespacedKey(this, "addon_category");
         ItemGroup itemGroup = new ItemGroup(itemGroupId, itemGroupItem);
 
         /*
-         * 2. Create a new SlimefunItemStack
-         * This class has many constructors, it is very important
-         * that you give each item a unique id.
+         * 2. 创建一个 SlimefunItemStack
+         * 这个类是 ItemStack 的扩展，拥有多个构造函数
+         * 重要：每个物品都得有一个独一无二的ID
          */
-        SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4Cool Diamond", "&c+20% Coolness");
+        SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4炫酷的钻石", "&c+20% 炫酷");
 
         /*
-         * 3. Creating a Recipe
-         * The Recipe is an ItemStack Array with a length of 9.
-         * It represents a Shaped Recipe in a 3x3 crafting grid.
-         * The machine in which this recipe is crafted in is specified
-         * further down as the RecipeType.
+         * 3. 创建配方
+         * 这个配方是一个拥有9个ItemStack的数组。
+         * 它代表了一个3x3的有序合成配方。
+         * 该配方所需的机器将在后面通过RecipeType指定。
          */
         ItemStack[] recipe = { new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.DIAMOND), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD) };
 
         /*
-         * 4. Registering the Item
-         * Now you just have to register the item.
-         * RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in
-         * which this item is crafted in.
-         * Recipe Types from Slimefun itself will automatically add the recipe to that machine.
+         * 4. 注册物品
+         * 现在，你只需要注册物品
+         * RecipeType.ENHANCED_CRAFTING_TABLE 代表
+         * 该物品将在增强型工作台中合成。
+         * 来自粘液科技本体的配方类型将会自动将配方添加到对应的机器中。
          */
         SlimefunItem item = new SlimefunItem(itemGroup, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
         item.register(this);
@@ -63,20 +62,20 @@ public class ExampleAddon extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onDisable() {
-        // Logic for disabling the plugin...
+        // 禁用插件的逻辑...
     }
 
     @Override
     public String getBugTrackerURL() {
-        // You can return a link to your Bug Tracker instead of null here
+        // 你可以在这里返回你的问题追踪器的网址，而不是 null
         return null;
     }
 
     @Override
     public JavaPlugin getJavaPlugin() {
         /*
-         * You will need to return a reference to your Plugin here.
-         * If you are using your main class for this, simply return "this".
+         * 你需要返回对你插件的引用。
+         * 如果这是你插件的主类，只需要返回 "this" 即可。
          */
         return this;
     }
